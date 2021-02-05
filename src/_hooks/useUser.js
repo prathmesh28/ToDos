61b7ae48a693dfa15880107d140f822b81f7ac9c
Prathmesh28 <prathmeshkerkar10@gmail.com>
2021-02-05 12:18:43 +0530

    updated

--- src/_hooks/useUser.js
+++ src/_hooks/useUser.js
@@ -1,9 +1,7 @@
 import * as React from "react";
 import rxUser from "../helpers/rxUser";
-/**
- * The reason we say "use User" is because React hooks always
- * start with the word "use" as we are "using" the "user" object from firebase.
- */
+
+
 export default function useUser() {
   const [userState, setUserState] = React.useState(null);
   const [loaded, setLoaded] = React.useState(false);
